d5e43930e8cea13f739758764e258a37d14c5146
sullentx <233290@ids.upchiapas.edu.mx>
2025-04-18 09:32:59 -0600

    header ready

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -6,6 +6,7 @@ import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { CommonModule } from '@angular/common';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-main-layout',
@@ -14,11 +15,11 @@ import { CommonModule } from '@angular/common';
   standalone: true,
     imports: [MatCommonModule, SidebarComponent, 
         MatLineModule,
-        RouterOutlet,MatToolbarModule,MatSidenavModule,CommonModule],
+        RouterOutlet,MatToolbarModule,MatSidenavModule,CommonModule, MatButton],
 })
 export class MainLayoutComponent {
   sidenavExpanded = true;
-
+  userName = 'Eduardo Uriel'; // Example user name
   toggleSidenav(): void {
     this.sidenavExpanded = !this.sidenavExpanded;
   }
